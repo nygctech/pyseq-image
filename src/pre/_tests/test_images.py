28211a249aa26d70f0976d244bf6c75a674d1f4b
tommyly201 <60830969+tommyly201@users.noreply.github.com>
2022-08-04 15:07:34 -0400

    update test_image.py

--- src/pre/_tests/test_images.py
+++ src/pre/_tests/test_images.py
@@ -1,18 +1,25 @@
-from pyseq import image_analysis as ia
+from pre import image_analysis as ia
 import numpy as np
+import pytest
+from pathlib import Path
+
+image_path = '/nethome/kpandit/NYGC-PySeq2500-Pipeline/src/demo/'
+
+@pytest.fixture
+def demo_image():
+    image_path = Path(ia.__file__).parents[2] / Path('src/demo/images')
+    # Reads 2 sections; m1a and m3b, return 1 it doesn't matter which
+    ims = ia.get_HiSeqImage(image_path) 
+
+    return ims[0]
 
 
-# FIX IMAGE PATH
-image_path = '/Users/tommyly/image_path/src/demo/images'
 
 def test_correct_background():
 
-    image_path = '/Users/tommyly/image_path/src/demo/images'
-    image = ia.get_HiSeqImages(image_path)
-    image_2 = image[0]
+    raw_image = demo_image.im
+    corrected_im = demo_image.correct_background_2()
 
-    corrected_im = cb.correct_background_2(image[0])
-
-    assert corrected_im.shape  == image_2.im.shape
+    assert corrected_im.shape  == raw_image.shape
     assert np.max(corrected_im) == 4095
     assert np.min(corrected_im) >= 0
